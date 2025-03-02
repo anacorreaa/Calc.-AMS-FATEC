aedc8fec343b9a22476ac1d36a86a9545fbbe258
anacorreaa <anacarolcorr07@gmail.com>
2025-03-02 18:49:26 -0300

    Primeiro commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-form',
   standalone: false,
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+  //somar
+  onClickSum(){
+    this.result = this.num1 + this.num2;
+  }
+  onClickSub(){
+    this.result = this.num1 - this.num2;
+  }
+  onClickDiv(){
+    this.result = this.num1 / this.num2;
+  }
+  onClickMult(){
+    this.result = this.num1 * this.num2;
+  }
+  onClickPot(){
+    this.result = Math.pow(this.num1, this.num2);
+  }
+  onClickMed(){
+    this.result = (this.num1 + this.num2) / 2;
+  }
 
 }
